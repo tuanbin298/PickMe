9996354556f42860683dbfd5582cf4a4641287bd
tuanbin298 <tuannase171419@fpt.edu.vn>
2025-09-12 20:47:10 +0700

    Setup

---
+++ pickme_fe_app/lib/pages/login.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Logo
+              Image.asset("assets/images/pickme_logo.png"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

ba26747357ecdc9fef27f1269165f782c6b51add
tuanbin298 <tuannase171419@fpt.edu.vn>
2025-09-13 22:15:01 +0700

    Complete UI login

--- pickme_fe_app/lib/pages/login.dart
+++ pickme_fe_app/lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pickme_fe_app/pages/register.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,18 +9,209 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool _obscurePassword = true;
+
+  // Controller to get data from inputs
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  // Release memory that controller stored
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40),
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               // Logo
-              Image.asset("assets/images/pickme_logo.png"),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 1.0),
+                child: Image.asset(
+                  "lib/assets/images/pickme_logo.png",
+                  height: 250,
+                ),
+              ),
+
+              // Text Welcome
+              const Text(
+                "Chào mừng bạn",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF2D3142),
+                ),
+              ),
+
+              Column(
+                children: [
+                  const Text(
+                    "Đăng nhập để tiếp tục",
+                    style: TextStyle(fontSize: 16, color: Colors.black54),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Hoặc",
+                        style: TextStyle(fontSize: 16, color: Colors.black54),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (_) => Register()),
+                          );
+                        },
+                        child: const Text(
+                          "Tạo tài khoản",
+                          style: TextStyle(fontSize: 16, color: Colors.orange),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 24),
+
+              // Phone input
+              TextField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  hintText: "Số điện thoại",
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 16,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 14),
+
+              // Password input
+              TextField(
+                controller: passwordController,
+                obscureText: _obscurePassword,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
+                    icon: Icon(
+                      _obscurePassword
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                    ),
+                  ),
+                  hintText: "Mật khẩu",
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 16,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 14),
+
+              // Login button
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 115,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: const Text(
+                  "Đăng nhập",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+
+              const SizedBox(height: 14),
+
+              // Forgot password
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Quên mật khẩu?",
+                    style: TextStyle(color: Colors.orange),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 8),
+
+              // Divider
+              Row(
+                children: const [
+                  Expanded(child: Divider(thickness: 1, color: Colors.black26)),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: Text(
+                      "hoặc",
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                  ),
+                  Expanded(child: Divider(thickness: 1, color: Colors.black26)),
+                ],
+              ),
+
+              const SizedBox(height: 14),
+
+              // Google login
+              OutlinedButton.icon(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Colors.grey[100],
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 14,
+                    horizontal: 30,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                label: const Text(
+                  "Đăng nhập bằng Google",
+                  style: TextStyle(fontSize: 16, color: Colors.black87),
+                ),
+                icon: Image.asset(
+                  "lib/assets/logo/google_logo.png",
+                  height: 24,
+                ),
+              ),
+
+              const SizedBox(height: 30),
             ],
           ),
         ),

5b3086a8db5d896b454d753ba828960fb81043bf
tuanbin298 <tuannase171419@fpt.edu.vn>
2025-10-28 12:59:39 +0700

    update

--- pickme_fe_app/lib/features/customer/screens/restaurant/restaurant_menu_page.dart
+++ pickme_fe_app/lib/features/customer/screens/restaurant/restaurant_menu_page.dart
@@ -4,7 +4,7 @@ import '../../../customer/models/restaurant/restaurant.dart';
 import '../../../customer/models/restaurant/restaurant_menu.dart';
 import '../../../customer/services/restaurant/restaurant_menu_service.dart';
 import 'package:pickme_fe_app/features/customer/widgets/restaurant/restaurant_menu_tab_view.dart';
-// import 'package:pickme_fe_app/core/common_services/utils_method.dart';
+import 'package:pickme_fe_app/core/common_services/utils_method.dart';
 
 class RestaurantMenuPage extends StatefulWidget {
   final Restaurant restaurant;
@@ -39,7 +39,7 @@ class _RestaurantMenuPageState extends State<RestaurantMenuPage>
   @override
   Widget build(BuildContext context) {
     final restaurant = widget.restaurant;
-    // final bool isClosed = !restaurant.isOpen;
+    final bool isClosed = !restaurant.isOpen;
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -94,84 +94,84 @@ class _RestaurantMenuPageState extends State<RestaurantMenuPage>
                         },
                       );
                     },
-                    // unavailable: isClosed,
+                    unavailable: isClosed,
                   ),
                 ),
 
                 // Notify user if restaurant is closed
-                // if (isClosed)
-                // Container(
-                //   width: double.infinity,
-                //   padding: const EdgeInsets.symmetric(
-                //     horizontal: 20,
-                //     vertical: 16,
-                //   ),
-                //   decoration: BoxDecoration(
-                //     color: Colors.white,
-                //     boxShadow: [
-                //       BoxShadow(
-                //         color: Colors.black12,
-                //         blurRadius: 8,
-                //         offset: const Offset(0, -2),
-                //       ),
-                //     ],
-                //     borderRadius: const BorderRadius.vertical(
-                //       top: Radius.circular(20),
-                //     ),
-                //   ),
+                if (isClosed)
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 8,
+                          offset: const Offset(0, -2),
+                        ),
+                      ],
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(20),
+                      ),
+                    ),
 
-                //   // Closed notification
-                //   child: Row(
-                //     crossAxisAlignment: CrossAxisAlignment.center,
-                //     mainAxisAlignment: MainAxisAlignment.center,
-                //     children: [
-                //       // Clock Icon
-                //       Container(
-                //         decoration: BoxDecoration(
-                //           color: Colors.orange.shade100,
-                //           shape: BoxShape.circle,
-                //         ),
-                //         padding: const EdgeInsets.all(8),
-                //         child: Icon(
-                //           Icons.access_time_filled_rounded,
-                //           color: Colors.orange.shade600,
-                //           size: 22,
-                //         ),
-                //       ),
+                    // Closed notification
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Clock Icon
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.orange.shade100,
+                            shape: BoxShape.circle,
+                          ),
+                          padding: const EdgeInsets.all(8),
+                          child: Icon(
+                            Icons.access_time_filled_rounded,
+                            color: Colors.orange.shade600,
+                            size: 22,
+                          ),
+                        ),
 
-                //       const SizedBox(width: 12),
+                        const SizedBox(width: 12),
 
-                //       // Closed message
-                //       Expanded(
-                //         child: Column(
-                //           crossAxisAlignment: CrossAxisAlignment.start,
-                //           mainAxisSize: MainAxisSize.min,
-                //           children: [
-                //             Text(
-                //               'Cửa hàng tạm ngưng nhận đơn',
-                //               style: TextStyle(
-                //                 color: Colors.orange.shade800,
-                //                 fontSize: 16,
-                //                 fontWeight: FontWeight.bold,
-                //               ),
-                //             ),
+                        // Closed message
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                'Cửa hàng tạm ngưng nhận đơn',
+                                style: TextStyle(
+                                  color: Colors.orange.shade800,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
 
-                //             const SizedBox(height: 4),
+                              const SizedBox(height: 4),
 
-                //             Text(
-                //               'Sẽ mở lại vào ${UtilsMethod.formatTime(restaurant.openingTime)}.',
-                //               style: TextStyle(
-                //                 color: Colors.grey.shade700,
-                //                 fontSize: 14,
-                //                 height: 1.3,
-                //               ),
-                //             ),
-                //           ],
-                //         ),
-                //       ),
-                //     ],
-                //   ),
-                // ),
+                              Text(
+                                'Sẽ mở lại vào ${UtilsMethod.formatTime(restaurant.openingTime)}.',
+                                style: TextStyle(
+                                  color: Colors.grey.shade700,
+                                  fontSize: 14,
+                                  height: 1.3,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
               ],
             );
           },
